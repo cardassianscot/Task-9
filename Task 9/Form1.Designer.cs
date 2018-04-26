47b7b35f6a69734a7a2caae9a29302fe58903c64
David Petticrew <cardassianscot@gmail.com>
2018-04-26 15:36:07 +0100

    Prep for assignemnt

--- Task 9/Form1.Designer.cs
+++ Task 9/Form1.Designer.cs
@@ -28,42 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.polynomialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.yxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.yxToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.yxToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.trigToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.cosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sinAndCosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(0, 52);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(800, 398);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -73,8 +66,7 @@
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.polynomialToolStripMenuItem,
-            this.trigToolStripMenuItem});
+            this.polynomialToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 52);
@@ -83,19 +75,9 @@
             // 
             // fileToolStripMenuItem
             // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem,
-            this.saveToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(75, 48);
             this.fileToolStripMenuItem.Text = "File";
-            // 
-            // openToolStripMenuItem
-            // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(396, 46);
-            this.openToolStripMenuItem.Text = "Open";
-            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // polynomialToolStripMenuItem
             // 
@@ -110,73 +92,27 @@
             // yxToolStripMenuItem
             // 
             this.yxToolStripMenuItem.Name = "yxToolStripMenuItem";
-            this.yxToolStripMenuItem.Size = new System.Drawing.Size(193, 46);
+            this.yxToolStripMenuItem.Size = new System.Drawing.Size(396, 46);
             this.yxToolStripMenuItem.Text = "y=x";
             this.yxToolStripMenuItem.Click += new System.EventHandler(this.yxToolStripMenuItem_Click);
             // 
             // yxToolStripMenuItem1
             // 
             this.yxToolStripMenuItem1.Name = "yxToolStripMenuItem1";
-            this.yxToolStripMenuItem1.Size = new System.Drawing.Size(193, 46);
+            this.yxToolStripMenuItem1.Size = new System.Drawing.Size(396, 46);
             this.yxToolStripMenuItem1.Text = "y=x²";
             this.yxToolStripMenuItem1.Click += new System.EventHandler(this.yxToolStripMenuItem1_Click);
             // 
             // yxToolStripMenuItem2
             // 
             this.yxToolStripMenuItem2.Name = "yxToolStripMenuItem2";
-            this.yxToolStripMenuItem2.Size = new System.Drawing.Size(193, 46);
+            this.yxToolStripMenuItem2.Size = new System.Drawing.Size(396, 46);
             this.yxToolStripMenuItem2.Text = "y=x³";
             this.yxToolStripMenuItem2.Click += new System.EventHandler(this.yxToolStripMenuItem2_Click);
-            // 
-            // trigToolStripMenuItem
-            // 
-            this.trigToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.sinToolStripMenuItem,
-            this.cosToolStripMenuItem,
-            this.tanToolStripMenuItem,
-            this.sinAndCosToolStripMenuItem});
-            this.trigToolStripMenuItem.Name = "trigToolStripMenuItem";
-            this.trigToolStripMenuItem.Size = new System.Drawing.Size(78, 48);
-            this.trigToolStripMenuItem.Text = "Trig";
-            // 
-            // sinToolStripMenuItem
-            // 
-            this.sinToolStripMenuItem.Name = "sinToolStripMenuItem";
-            this.sinToolStripMenuItem.Size = new System.Drawing.Size(280, 46);
-            this.sinToolStripMenuItem.Text = "sin";
-            this.sinToolStripMenuItem.Click += new System.EventHandler(this.sinToolStripMenuItem_Click);
-            // 
-            // cosToolStripMenuItem
-            // 
-            this.cosToolStripMenuItem.Name = "cosToolStripMenuItem";
-            this.cosToolStripMenuItem.Size = new System.Drawing.Size(280, 46);
-            this.cosToolStripMenuItem.Text = "cos";
-            this.cosToolStripMenuItem.Click += new System.EventHandler(this.cosToolStripMenuItem_Click);
-            // 
-            // tanToolStripMenuItem
-            // 
-            this.tanToolStripMenuItem.Name = "tanToolStripMenuItem";
-            this.tanToolStripMenuItem.Size = new System.Drawing.Size(280, 46);
-            this.tanToolStripMenuItem.Text = "tan";
-            this.tanToolStripMenuItem.Click += new System.EventHandler(this.tanToolStripMenuItem_Click);
-            // 
-            // sinAndCosToolStripMenuItem
-            // 
-            this.sinAndCosToolStripMenuItem.Name = "sinAndCosToolStripMenuItem";
-            this.sinAndCosToolStripMenuItem.Size = new System.Drawing.Size(280, 46);
-            this.sinAndCosToolStripMenuItem.Text = "sin and cos";
-            this.sinAndCosToolStripMenuItem.Click += new System.EventHandler(this.sinAndCosToolStripMenuItem_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // saveToolStripMenuItem
-            // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(396, 46);
-            this.saveToolStripMenuItem.Text = "Save";
-            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -204,16 +140,9 @@
         private System.Windows.Forms.ToolStripMenuItem yxToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem yxToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem yxToolStripMenuItem2;
-        private System.Windows.Forms.ToolStripMenuItem trigToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem sinToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem cosToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem tanToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem sinAndCosToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
-        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
     }
 }
 
--- Task 9/Form1.cs
+++ Task 9/Form1.cs
@@ -32,42 +32,8 @@ namespace Task_9
             }
         }
 
-        void drawGraph(List<Point> points, string XAxisTitle = "x", string YAxisTitle = "y", bool PlotPoints = true)
-        {
-            chart1.Series.Clear();
-            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
-            Series series1 = new Series
-            {
-                Name = "Line",
-                Color = Color.Blue,
-                IsVisibleInLegend = false,
-                IsXValueIndexed = true,
-                ChartType = SeriesChartType.Spline,
-                BorderWidth = 2
-            };
-            Series series2 = new Series
-            {
-                Name = "Points",
-                Color = Color.Red,
-                IsVisibleInLegend = false,
-                IsXValueIndexed = true,
-                ChartType = SeriesChartType.Point,
-                BorderWidth = 2
-            };
-            chart1.Series.Add(series1);
-            if (PlotPoints)
-                chart1.Series.Add(series2);
-            foreach (Point p in points)
-            {
-                series1.Points.AddXY(p.X, p.Y);
-                series2.Points.AddXY(p.X, p.Y);
-            }
-            chart1.ChartAreas[0].AxisX.Title = XAxisTitle;
-            chart1.ChartAreas[0].AxisY.Title = YAxisTitle;
-            chart1.ChartAreas[0].RecalculateAxesScale();
-        }
 
-        void drawGraphDouble(List<PointD> points, string XAxisTitle = "x", string YAxisTitle = "y", bool PlotPoints = true)
+        void drawGraph(List<PointD> points, string XAxisTitle = "x", string YAxisTitle = "y", bool PlotPoints = true)
         {
             chart1.Series.Clear();
             chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
@@ -104,166 +70,33 @@ namespace Task_9
 
         private void yxToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            List<Point> linear = new List<Point>();
+            List<PointD> linear = new List<PointD>();
             for (int i = 0; i < 11; i++)
             {
-                linear.Add(new Point(i, i));
+                linear.Add(new PointD(i, i));
             }
             drawGraph(linear);
         }
 
         private void yxToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            List<Point> squares = new List<Point>();
+            List<PointD> squares = new List<PointD>();
             for (int i = 0; i < 11; i++)
             {
-                squares.Add(new Point(i, i * i));
+                squares.Add(new PointD(i, i * i));
             }
             drawGraph(squares);
         }
 
         private void yxToolStripMenuItem2_Click(object sender, EventArgs e)
         {
-            List<Point> cubes = new List<Point>();
+            List<PointD> cubes = new List<PointD>();
             for (int i = 0; i < 11; i++)
             {
-                cubes.Add(new Point(i, i * i * i));
+                cubes.Add(new PointD(i, i * i * i));
             }
             drawGraph(cubes);
         }
-
-        private void sinToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            List<PointD> sines = new List<PointD>();
-            for (double i = 0; i < 361; i++)
-            {
-                sines.Add(new PointD(i, sin(i)));
-            }
-            drawGraphDouble(sines, "θ", "sin θ", false);
-        }
-
-        private double sin(double θ)
-        {
-            return Math.Sin(θ * Math.PI / 180.0);
-        }
-
-        private double cos(double θ)
-        {
-            return Math.Cos(θ * Math.PI / 180.0);
-        }
-
-        private double tan(double θ)
-        {
-            return Math.Tan(θ * Math.PI / 180.0);
-        }
-
-        private void cosToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            List<PointD> cosines = new List<PointD>();
-            for (double i = 0; i < 361; i++)
-            {
-                cosines.Add(new PointD(i, cos(i)));
-            }
-            drawGraphDouble(cosines, "θ", "cos θ", false);
-        }
-
-        private void tanToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            List<PointD> tans = new List<PointD>();
-            for (double i = 0; i < 361; i++)
-            {
-                if (Math.Abs(tan(i))<10.0)
-                    tans.Add(new PointD(i, tan(i)));
-            }
-            drawGraphDouble(tans, "θ", "tan θ", false);
-            
-        }
-
-        private void sinAndCosToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            chart1.Series.Clear();
-            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
-            Series series1 = new Series
-            {
-                Name = "Cos θ",
-                Color = Color.Blue,
-                IsVisibleInLegend = true,
-                IsXValueIndexed = true,
-                ChartType = SeriesChartType.Spline,
-                BorderWidth = 2
-            };
-            Series series2 = new Series
-            {
-                Name = "Sin θ",
-                Color = Color.Red,
-                IsVisibleInLegend = true,
-                IsXValueIndexed = true,
-                ChartType = SeriesChartType.Spline,
-                BorderWidth = 2
-            };
-            chart1.Series.Add(series1);
-            chart1.Series.Add(series2);
-            for (double i = 0; i < 361; i++)
-            {
-                series1.Points.AddXY(i, cos(i));
-                series2.Points.AddXY(i, sin(i));
-            }
-            chart1.ChartAreas[0].RecalculateAxesScale();
-        }
-
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.FileName = "";
-            openFileDialog1.Filter = "CSV Files|*.csv";
-            DialogResult result = openFileDialog1.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                try
-                {
-                    List<PointD> points = new List<PointD>();
-                    string[] titles;
-                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
-                    {
-                        titles = sr.ReadLine().Split(',');
-                        while (!sr.EndOfStream)
-                        {
-                            string[] l = sr.ReadLine().Split(',');
-                            points.Add(new PointD(double.Parse(l[0]), double.Parse(l[1])));
-                        }
-                    }
-                    drawGraphDouble(points, titles[0], titles[1]);
-                }
-                catch(IOException)
-                {
-                    MessageBox.Show(openFileDialog1.FileName + " failed to open.");
-                }
-                catch (FormatException)
-                {
-                    MessageBox.Show(openFileDialog1.FileName + " is in the wrong format.");
-                }
-                catch(IndexOutOfRangeException)
-                {
-                    MessageBox.Show(openFileDialog1.FileName + " is in the wrong format.");
-                }
-            }
-        }
-
-        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            saveFileDialog1.FileName = "";
-            saveFileDialog1.Filter = "PNG Files|*.PNG";
-            DialogResult result = saveFileDialog1.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                try
-                {
-                    chart1.SaveImage(saveFileDialog1.FileName, ChartImageFormat.Png);
-                }
-                catch
-                {
-                    MessageBox.Show(saveFileDialog1.FileName, " failed to save.");
-                }
-            }
-        }
+        
     }
 }
